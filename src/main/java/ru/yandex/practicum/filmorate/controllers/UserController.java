178e6f6867f2c2e55cadf27b5769b897116dcbd2
Username <maximorechov@yandex.ru>
2022-07-11 23:08:39 +0500

    remove: вынесена логика по обработке запросов. только эндпойнты

--- src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -1,80 +1,41 @@
 package ru.yandex.practicum.filmorate.controllers;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exceptions.InvalidEmailException;
-import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
-import javax.servlet.http.HttpServletRequest;
-import java.time.LocalDate;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
-@Slf4j
 public class UserController {
-    private final Map<Long, User> users = new HashMap<>();
-    private long id = 0;
 
+    private final UserStorage userStorage;
+
+    public UserController(UserStorage userStorage) {
+        this.userStorage = userStorage;
+    }
 
     @GetMapping
     public Collection<User> findAll() {
-        log.debug("Текущее количество пользователей: {}", users.size());
-        return users.values();
+        return userStorage.allUsers();
     }
 
     @PostMapping
-    public User create(@RequestBody User user, HttpServletRequest request) {
-        validate(user);
-        user.setId(++id);
-        users.put(id, user);
-        log.info("Получен запрос к эндпоинту: '{} {}', Строка параметров запроса: '{}'",
-                request.getMethod(), request.getRequestURI(), request.getQueryString());
-        return users.get(id);
+    public User create(@RequestBody User user) {
+        return userStorage.addUser(user);
     }
 
     @PutMapping
     public User put(@RequestBody User user) {
-        if (users.containsKey(user.getId())) {
-            validate(user);
-            users.put(user.getId(), user);
-            log.debug("Текущее количество пользователей: {}", users.size());
-            return user;
-        } else {
-            throw new ValidationException("Пользователь с таким Id  не существует.");
-        }
+        return userStorage.changeUser(user);
     }
 
-    public void validate(User user) {
-
-        if (user.getEmail() == null || user.getEmail().isBlank()) {
-            throw new InvalidEmailException("Адрес электронной почты не может быть пустым.");
-        }
-
-        if (!user.getEmail().contains("@")) {
-            throw new InvalidEmailException("Адрес электронной почты должен содержать символ @.");
-        }
-
-        if (user.getLogin() == null || user.getLogin().isBlank()) {
-            throw new ValidationException("Login не может быть пустым.");
-        }
-
-        if (user.getLogin().contains(" ")) {
-            throw new ValidationException("Login не может содержать пробелы.");
-        }
-
-        if (user.getName().isEmpty()) {
-            user.setName(user.getLogin());
-        }
-
-        if (user.getBirthday().compareTo(LocalDate.now()) > 0) {
-            throw new ValidationException("Пользователь еще не родился.");
-        }
-
+    @DeleteMapping
+    public void removeUser(@RequestBody Long idUser) {
+        userStorage.removeUser(idUser);
     }
+
 }
 
 
