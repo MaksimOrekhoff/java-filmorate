c428129a0a2d692419e5d9fb66a3b5cd2ea4ff5b
Username <maximorechov@yandex.ru>
2022-07-14 01:16:19 +0500

    add: обработчик исключений и новые классы для них

---
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/ErrorHandler.java
@@ -0,0 +1,35 @@
+package ru.yandex.practicum.filmorate.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.yandex.practicum.filmorate.exceptions.FilmNotFoundException;
+import ru.yandex.practicum.filmorate.exceptions.UserNotFoundException;
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
+import ru.yandex.practicum.filmorate.model.ErrorResponse;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse invalidEmailException(final ValidationException validationException) {
+        return new ErrorResponse(validationException.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String filmNotFoundException(final FilmNotFoundException filmNotFoundException) {
+        return filmNotFoundException.getMessage();
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String userNotFoundException(final UserNotFoundException e) {
+        return e.getMessage();
+    }
+}
