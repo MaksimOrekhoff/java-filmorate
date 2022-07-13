62d360db7c692f1cfda3d07d2fd4f0943a9cc02c
Username <maximorechov@yandex.ru>
2022-07-14 01:17:18 +0500

    add: поля для хранения списка друзей и лайков

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -3,6 +3,8 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -14,4 +16,17 @@ public class Film {
     private String description;
     private LocalDate releaseDate;
     private long duration;
+    private Set<Long> likes = new HashSet<>();
+
+    @Override
+    public String toString() {
+        return "Film{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", releaseDate=" + releaseDate +
+                ", duration=" + duration +
+                ", likes=" + likes +
+                '}';
+    }
 }
