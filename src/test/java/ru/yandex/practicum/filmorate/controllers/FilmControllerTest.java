ef54c3959261a4bd61b9a5b3695b3db32b908a75
Username <maximorechov@yandex.ru>
2022-07-14 01:16:53 +0500

    refactor: покрытие юнит тестов

--- src/test/java/ru/yandex/practicum/filmorate/controllers/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controllers/FilmControllerTest.java
@@ -1,42 +1,43 @@
 package ru.yandex.practicum.filmorate.controllers;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.film.FilmService;
+import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
 
 import java.time.LocalDate;
+import java.util.HashSet;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FilmControllerTest {
-    FilmController  filmController = new FilmController();
+    FilmService filmService = new FilmService(new InMemoryFilmStorage());
 
     @Test
     void validateEmailIsEmpty() {
-        final Film film = new Film(1, "", "Drama", LocalDate.of(1996, 7, 3), 120);
-        assertThrows(ValidationException.class, () -> filmController.validate(film));
+        final Film film = new Film(1, "", "Drama", LocalDate.of(1996, 7, 3), 120, new HashSet<>());
+        assertThrows(ValidationException.class, () -> filmService.validate(film));
     }
 
     @Test
     void validateDescriptionFilmMore200() {
         String description = "11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111" +
                 "11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111";
-        final Film film = new Film(1, "Name", description, LocalDate.of(1996, 7, 3), 120);
-        assertThrows(ValidationException.class, () -> filmController.validate(film));
+        final Film film = new Film(1, "Name", description, LocalDate.of(1996, 7, 3), 120, new HashSet<>());
+        assertThrows(ValidationException.class, () -> filmService.validate(film));
     }
 
     @Test
     void validateReleaseFilmIsAfter1895() {
-        final Film film = new Film(1, "Name", "Drama", LocalDate.of(1796, 7, 3), 120);
-        assertThrows(ValidationException.class, () -> filmController.validate(film));
+        final Film film = new Film(1, "Name", "Drama", LocalDate.of(1796, 7, 3), 120, new HashSet<>());
+        assertThrows(ValidationException.class, () -> filmService.validate(film));
     }
 
     @Test
     void validateDurationFilmIsNegative() {
-        final Film film = new Film(1, "Name", "Drama", LocalDate.of(1996, 7, 3), -120);
-        assertThrows(ValidationException.class, () -> filmController.validate(film));
+        final Film film = new Film(1, "Name", "Drama", LocalDate.of(1996, 7, 3), -120, new HashSet<>());
+        assertThrows(ValidationException.class, () -> filmService.validate(film));
     }
 
 }
--- src/test/java/ru/yandex/practicum/filmorate/controllers/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controllers/UserControllerTest.java
@@ -4,41 +4,44 @@ import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.exceptions.InvalidEmailException;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.user.UserService;
+import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
 
 import java.time.LocalDate;
+import java.util.HashSet;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserControllerTest {
-    UserController userController = new UserController();
+    UserService userService = new UserService(new InMemoryUserStorage());
 
     @Test
     void validateEmailIsEmpty() {
-        final User user = new User(1, "", "Login", "User", LocalDate.of(1996, 7, 3));
-        assertThrows(InvalidEmailException.class, () -> userController.validate(user));
+        final User user = new User(1, "", "Login", "User", LocalDate.of(1996, 7, 3), new HashSet<>());
+        assertThrows(InvalidEmailException.class, () -> userService.validate(user));
     }
 
     @Test
     void validateEmailIsNotContainsDog() {
-        final User user = new User(1, "newUseremail", "Login", "User", LocalDate.of(1996, 7, 3));
-        assertThrows(InvalidEmailException.class, () -> userController.validate(user));
+        final User user = new User(1, "newUserEmail", "Login", "User", LocalDate.of(1996, 7, 3), new HashSet<>());
+        assertThrows(InvalidEmailException.class, () -> userService.validate(user));
     }
 
     @Test
     void validateLoginIsEmpty() {
-        final User user = new User(1, "newUser@email", "", "User", LocalDate.of(1996, 7, 3));
-        assertThrows(ValidationException.class, () -> userController.validate(user));
+        final User user = new User(1, "newUser@email", "", "User", LocalDate.of(1996, 7, 3), new HashSet<>());
+        assertThrows(ValidationException.class, () -> userService.validate(user));
     }
 
     @Test
     void validateLoginIsContainsSpace() {
-        final User user = new User(1, "newUser@email", "Us er", "User", LocalDate.of(1996, 7, 3));
-        assertThrows(ValidationException.class, () -> userController.validate(user));
+        final User user = new User(1, "newUser@email", "Us er", "User", LocalDate.of(1996, 7, 3), new HashSet<>());
+        assertThrows(ValidationException.class, () -> userService.validate(user));
     }
 
     @Test
     void validateBirthdayIsFuture() {
-        final User user = new User(1, "newUser@email", "User", "User", LocalDate.of(2996, 7, 3));
-        assertThrows(ValidationException.class, () -> userController.validate(user));
+        final User user = new User(1, "newUser@email", "User", "User", LocalDate.of(2996, 7, 3), new HashSet<>());
+        assertThrows(ValidationException.class, () -> userService.validate(user));
     }
 }
