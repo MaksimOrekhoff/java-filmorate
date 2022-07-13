8c5af20a864c08ee226956871dce0d909a958abe
Username <maximorechov@yandex.ru>
2022-07-14 01:18:32 +0500

    add: изменены зависимости. вынесена логика хранения. только эндпойнты

--- src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
@@ -1,35 +1,66 @@
 package ru.yandex.practicum.filmorate.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+import ru.yandex.practicum.filmorate.service.film.FilmService;
+
 import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/films")
 public class FilmController {
-    private final FilmStorage filmStorage;
+    private final FilmService filmService;
 
-    public FilmController(FilmStorage filmStorage) {
-        this.filmStorage = filmStorage;
+    @Autowired
+    public FilmController(FilmService filmService) {
+        this.filmService = filmService;
     }
 
     @GetMapping
-    public Collection<Film> findAll() {
-        return filmStorage.allFilms();
+    public Collection<Film> findAllFilms() {
+        return filmService.allFilms();
+    }
+
+    @GetMapping("/{id}")
+    public Film findFilm(@PathVariable Long id) {
+        return filmService.getFilm(id);
+    }
+
+    @GetMapping("/popular")
+    public List<Film> topPopularFilms(@RequestParam(required = false) Integer count) {
+        return filmService.allPopFilms(count);
+    }
+
+    @PutMapping("/{id}/like/{userId}")
+    public Film userSetLike(@PathVariable Long id, @PathVariable Long userId) {
+        return filmService.setLike(id, userId);
     }
 
     @PostMapping
     public Film create(@RequestBody Film film) {
-        return filmStorage.addFilm(film);
+        return filmService.addFilm(film);
     }
 
     @PutMapping
-    public Film put(@RequestBody Film film) {
-        return filmStorage.changeFilm(film);
+    public Film changeFilm(@RequestBody Film film) {
+        return filmService.changesFilm(film);
     }
 
-    @DeleteMapping
-    public void deleteFilm(@RequestBody Long idFilm) {
-        filmStorage.removeFilm(idFilm);
+    @DeleteMapping("/{idFilm}")
+    public void deletesFilm(@PathVariable Long idFilm) {
+        filmService.removeFilm(idFilm);
+    }
+
+    @DeleteMapping("/{id}/like/{userId}")
+    public void userRemoveLike(@PathVariable Long id, @PathVariable Long userId) {
+        filmService.removeLike(id, userId);
+    }
+
+    @ExceptionHandler
+    public Map<String, String> handleNegativeCount(final IllegalArgumentException e) {
+        return Map.of("error", "Передан отрицательный параметр count.");
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -2,7 +2,7 @@ package ru.yandex.practicum.filmorate.controllers;
 
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.storage.user.UserStorage;
+import ru.yandex.practicum.filmorate.service.user.UserService;
 
 import java.util.Collection;
 
@@ -10,32 +10,57 @@ import java.util.Collection;
 @RequestMapping("/users")
 public class UserController {
 
-    private final UserStorage userStorage;
+    private final UserService userService;
 
-    public UserController(UserStorage userStorage) {
-        this.userStorage = userStorage;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping
     public Collection<User> findAll() {
-        return userStorage.allUsers();
+        return userService.allUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User findUser(@PathVariable Long id) {
+        return userService.getUser(id);
+    }
+
+
+    @GetMapping("/{id}/friends/common/{otherId}")
+    public Collection<User> mutualFriends(@PathVariable Long id, @PathVariable Long otherId) {
+        return userService.allMutualFriends(id, otherId);
+    }
+
+    @GetMapping("/{id}/friends")
+    public Collection<User> getAllFriendsUser(@PathVariable Long id) {
+        return userService.allFriendsUser(id);
     }
 
     @PostMapping
     public User create(@RequestBody User user) {
-        return userStorage.addUser(user);
+        return userService.addUser(user);
     }
 
     @PutMapping
     public User put(@RequestBody User user) {
-        return userStorage.changeUser(user);
+        return userService.changeUser(user);
     }
 
-    @DeleteMapping
-    public void removeUser(@RequestBody Long idUser) {
-        userStorage.removeUser(idUser);
+    @PutMapping("/{id}/friends/{friendId}")
+    public void addInFriends(@PathVariable Long id, @PathVariable Long friendId) {
+        userService.addInFriends(id, friendId);
     }
 
+    @DeleteMapping("/{idUser}")
+    public void removeUser(@PathVariable Long idUser) {
+        userService.removeUser(idUser);
+    }
+
+    @DeleteMapping("/{id}/friends/{friendId}")
+    public void removeUserInFriends(@PathVariable Long id, @PathVariable Long friendId) {
+        userService.deleteUserInFriends(id, friendId);
+    }
 }
 
 
