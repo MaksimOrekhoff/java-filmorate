e67a688e4668da18d4bad73135a5042bb5ed6525
Username <maximorechov@yandex.ru>
2022-07-15 21:11:10 +0500

    remove : удалены избыточные импорты

--- src/test/java/ru/yandex/practicum/filmorate/controllers/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controllers/FilmControllerTest.java
@@ -3,7 +3,7 @@ package ru.yandex.practicum.filmorate.controllers;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.service.film.FilmService;
+import ru.yandex.practicum.filmorate.service.FilmService;
 import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
 
 import java.time.LocalDate;
