3197fb4d1f1e70a9edc6d76ed0679a302228096a
Username <maximorechov@yandex.ru>
2022-07-15 21:09:01 +0500

    add: annotation валидации

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -2,6 +2,9 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
@@ -12,8 +15,11 @@ import java.util.Set;
 @NoArgsConstructor
 public class Film {
     private long id;
+    @NotNull
     private String name;
+    @NotNull
     private String description;
+    @NotNull
     private LocalDate releaseDate;
     private long duration;
     private Set<Long> likes = new HashSet<>();
