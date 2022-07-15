c48bc3d9c31c9ededfdfd8002dc3b7f8d33b0410
Username <maximorechov@yandex.ru>
2022-07-15 21:09:48 +0500

    fix: отдельный пакет

--- src/main/java/ru/yandex/practicum/filmorate/handlers/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/handlers/ErrorHandler.java
@@ -1,4 +1,4 @@
-package ru.yandex.practicum.filmorate.controllers;
+package ru.yandex.practicum.filmorate.handlers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
