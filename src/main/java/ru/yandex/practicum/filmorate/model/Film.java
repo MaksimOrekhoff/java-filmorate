4b783b423563ec6fa457edd9e38d515a3dde5650
Username <maximorechov@yandex.ru>
2022-07-17 19:12:07 +0500

    add : аннотированы поля для валидации

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -2,9 +2,9 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.*;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
@@ -15,12 +15,13 @@ import java.util.Set;
 @NoArgsConstructor
 public class Film {
     private long id;
-    @NotNull
+    @NotEmpty
     private String name;
     @NotNull
     private String description;
     @NotNull
     private LocalDate releaseDate;
+    @Positive
     private long duration;
     private Set<Long> likes = new HashSet<>();
 
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -2,6 +2,10 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.*;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
@@ -12,9 +16,14 @@ import java.util.Set;
 @NoArgsConstructor
 public class User {
     private long id;
+    @NotEmpty
+    @Email
     private String email;
+    @NotBlank
     private String login;
+    @NotNull
     private String name;
+    @NotNull
     private LocalDate birthday;
     private Set<Long> friends = new HashSet<>();
 }
